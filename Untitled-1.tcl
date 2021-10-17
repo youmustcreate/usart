90af712b784e2a63cb1cc880e4ed36303874ab70
zhc <yilaiwen7@gmail.com>
2021-10-18 03:33:23 +0800

    tcl

--- Untitled-1.tcl
+++ Untitled-1.tcl
@@ -31,7 +31,7 @@ puts "b = $b"
 
 
 #使用多行命令时，以最后命令行返回的结果为准
-set b [expr $a+16; set c 8]
+set b [expr $a+15; set c 9]
 #命令置换嵌套
 set d [expr [expr $a+16] + 4]
 puts "b = $b"
@@ -40,6 +40,22 @@ puts "d = $d"
 #% d = 28
 
 
+# set str1 "Hello World1"  
+# set str2 \n             
+# set str3 \x31           
+# puts "$str1 $str2 $str3"
+#% Hello World1 !
+#%  1
 
 
-
+namespace import ::tcl::mathfunc::*
+set pi 3.1415926
+# sin(pi/6) = 1/2
+puts "sin(pi/6) : [sin [expr $pi/6]]"
+# acos(0.5) = pi/3
+puts "acos(0.5) : [acos 0.5]"
+# atan(1) = pi/4
+puts "atant2(5/5) : [atan2 5 5]"
+#% sin(pi/6) : 0.49999999226497965
+#% acos(0.5) : 1.0471975511965979
+#% atant2(5/5) : 0.7853981633974483
